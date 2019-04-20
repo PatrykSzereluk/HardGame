fbad445c971a18be33e659e456c115c1f44849dc
PatrykSzereluk <patryk@boldpixel.com>
2019-04-20 20:31:03 +0200

    Added circle graphics, added player movement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+
+    private void Update()
+    {
+        GetInputs();
+    }
+
+    private void GetInputs()
+    {
+        Vector2 tmpPos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            tmpPos.y += speed *Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            tmpPos.y -= speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            tmpPos.x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            tmpPos.x += speed * Time.deltaTime;
+        }
+
+        transform.position = tmpPos;
+
+    }
+
+
+}
