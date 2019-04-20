52779e2abff7961ceb5733810ae8b04fab6ebfd0
PatrykSzereluk <patryk@boldpixel.com>
2019-04-20 21:16:04 +0200

    Added timer

---
+++ Assets/Script/Helper/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private float minutes;
+    private float milliseconds;
+    private float seconds;
+
+    [SerializeField]
+    private Text counterText;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minutes = (int)(Time.timeSinceLevelLoad / 60f) % 60;
+        milliseconds = (int)(Time.timeSinceLevelLoad * 1000f) % 1000;
+        seconds = (int)(Time.timeSinceLevelLoad % 60f);
+        counterText.text = minutes.ToString() + ":" + seconds.ToString() + ":" + milliseconds.ToString();
+
+    }
+}
